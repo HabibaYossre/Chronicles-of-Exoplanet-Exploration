596edfa4ed795afc2967d793dc9d5d329387eee6
Meefr <mohamedelramah737@gmail.com>
2024-09-21 18:01:18 +0300

    adding planets with animations

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -8,9 +8,10 @@ const Home = () => {
   return (
     <>
     <Navbar/>
-      <div className="flex w-full h-screen items-center justify-center absolute z-10">
+    <BgAnimation/>
+      {/* <div className="flex w-full h-screen items-center justify-center absolute z-10">
         <p className="text-white font-bold text-3xl md:text-6xl">EXOPLANETS WORLD</p>
-      </div>
+      </div> */}
       <div style={{ position: "relative", overflow: "hidden" }}>
         <video
           autoPlay
@@ -23,7 +24,7 @@ const Home = () => {
             width: "100%",
             height: "100vh",
             objectFit: "cover",
-            zIndex: -1,
+            zIndex: -1000,
           }}
         >
           <source
@@ -38,5 +39,36 @@ const Home = () => {
     </>
   );
 }
+
+function BgAnimation(){
+  return (
+    <section className="parallax overflow-hidden">
+      <img
+        src={require("../../Assets/HomePlanets/1.png")}
+        alt=""
+        id="planet-1"
+        className="w-[150px] end-[30px] top-28 planet-orbit3 "
+      />
+      <img
+        src={require("../../Assets/HomePlanets/2.png")}
+        alt=""
+        id="planet-2"
+        className="w-[150px] left-[-30px] top-1/2 planet-orbit2"
+      />
+      <div className="flex w-full h-screen items-center justify-center absolute z-10">
+        <p className="text-white font-bold text-3xl md:text-6xl">
+          EXOPLANETS WORLD
+        </p>
+      </div>
+      <img
+        src={require("../../Assets/HomePlanets/3.png")}
+        alt=""
+        id="planet-3"
+        className="w-[150px] left-[-30px] top-1/2 planet-orbit1"
+      />
+    </section>
+  );
+}
+
 
 export default Home
