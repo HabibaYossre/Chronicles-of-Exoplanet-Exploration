4fe355e82469d1f10a98acaf449ca246e13ef28c
Meefr <mohamedelramah737@gmail.com>
2024-09-21 18:31:43 +0300

    adding astronaut and new animation type

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './Home.css'
 import Footer from '../../Components/Footer/Footer';
 import Navbar from '../../Components/Navbar/Navbar';
@@ -40,23 +40,58 @@ const Home = () => {
   );
 }
 
-function BgAnimation(){
+function BgAnimation() {
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.pageYOffset;
+
+      // Adjust the speed and direction of each element based on scroll position
+      const planet1 = document.getElementById("planet-1");
+      const planet2 = document.getElementById("planet-2");
+      const planet3 = document.getElementById("planet-3");
+
+      if (planet1) {
+        planet1.style.transform = `translateX(${scrollPosition * -0.5}px)`; // Adjust speed as needed
+      }
+      if (planet2) {
+        planet2.style.transform = `translateX(${scrollPosition * -1.6}px)`; // Adjust speed as needed
+      }
+      if (planet3) {
+        planet3.style.transform = `translateX(${scrollPosition * 0.9}px)`; // Adjust speed as needed
+      }
+    };
+
+    // Attach scroll event listener
+    window.addEventListener("scroll", handleScroll);
+
+    // Cleanup on component unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <section className="parallax overflow-hidden">
+    <div className="parallax overflow-hidden">
       <img
         src={require("../../Assets/HomePlanets/1.png")}
         alt=""
         id="planet-1"
-        className="w-[150px] end-[30px] top-28 planet-orbit3 "
+        className="w-[120px] left-12 top-28 -z-30"
+      />
+      <img
+        src={require("../../Assets/HomePlanets/ast.png")}
+        alt=""
+        id="planet-1"
+        className="w-[200px] end-12 top-28 -z-30 ast"
       />
       <img
         src={require("../../Assets/HomePlanets/2.png")}
         alt=""
         id="planet-2"
-        className="w-[150px] left-[-30px] top-1/2 planet-orbit2"
+        className="w-[350px] left-[30px] top-80"
       />
       <div className="flex w-full h-screen items-center justify-center absolute z-10">
-        <p className="text-white font-bold text-3xl md:text-6xl">
+        <p className="text-white font-bold text-3xl md:text-6xl ">
           EXOPLANETS WORLD
         </p>
       </div>
@@ -64,11 +99,10 @@ function BgAnimation(){
         src={require("../../Assets/HomePlanets/3.png")}
         alt=""
         id="planet-3"
-        className="w-[150px] left-[-30px] top-1/2 planet-orbit1"
+        className="w-[150px] end-[30px] top-1/2 "
       />
-    </section>
+    </div>
   );
 }
-
 
 export default Home
