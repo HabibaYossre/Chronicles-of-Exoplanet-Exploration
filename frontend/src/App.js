589e2eeb9f07fa85ee2041783e91162fe16ec95f
Meefr <mohamedelramah737@gmail.com>
2024-09-23 20:25:16 +0300

    use lazyloading

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,47 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Home from './Pages/Home/Home';
-import PageNotFound from './Pages/PageNotFound/PageNotFound';
-import CardsManger from './Pages/Cards/CardsManger';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { lazy, Suspense } from "react";
 
+const LazyCardsManger = lazy(() => import("./Pages/Cards/CardsManger"));
+const LazyPageNotFound = lazy(() =>
+  import("./Pages/PageNotFound/PageNotFound")
+);
+const LazyHome = lazy(() => import("./Pages/Home/Home"));
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<PageNotFound />} />
-        <Route path="/cards" element={<CardsManger />} />
+        <Route
+          path="/"
+          element={
+            <Suspense
+              fallback={<div className="bg-black h-screen">Loading...</div>}
+            >
+              <LazyHome />
+            </Suspense>
+          }
+        />
+        <Route
+          path="*"
+          element={
+            <Suspense
+              fallback={<div className="bg-black h-screen">Loading...</div>}
+            >
+              <LazyPageNotFound />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/cards"
+          element={
+            <Suspense
+              fallback={<div className="bg-black h-screen">Loading...</div>}
+            >
+              <LazyCardsManger />
+            </Suspense>
+          }
+        />
       </Routes>
     </div>
   );
