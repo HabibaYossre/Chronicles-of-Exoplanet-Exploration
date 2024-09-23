e233643f3b5d3693b34c40209286d403e544e64c
Meefr <mohamedelramah737@gmail.com>
2024-09-23 18:05:58 +0300

    adding personal check in homepage section

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -3,16 +3,17 @@ import './Home.css'
 import Footer from '../../Components/Footer/Footer';
 import Navbar from '../../Components/Navbar/Navbar';
 import Slides from '../../Components/Slides/Slides';
+import { Button } from '@mui/material';
 
 const Home = () => {
   return (
-    <>
-    <Navbar/>
+    <div style={{ position: "relative", overflow: "hidden" }}>
+      <Navbar />
       {/* <div className="flex w-full h-screen items-center justify-center absolute z-10">
         <p className="text-white font-bold text-3xl md:text-6xl">EXOPLANETS WORLD</p>
         </div> */}
+      <BgAnimation />
       <div style={{ position: "relative", overflow: "hidden" }}>
-        <BgAnimation/>
         <video
           autoPlay
           loop
@@ -34,9 +35,10 @@ const Home = () => {
           Your browser does not support the video tag.
         </video>
       </div>
-      <Slides/>
-      <Footer/>
-    </>
+      <GoToPersonalCheck />
+      <Slides />
+      <Footer />
+    </div>
   );
 }
 
@@ -76,19 +78,19 @@ function BgAnimation() {
         src={require("../../Assets/HomePlanets/1.png")}
         alt=""
         id="planet-1"
-        className="w-[120px] left-12 top-28 -z-30 planet-orbit3"
+        className="w-[120px] left-12 top-28  planet-orbit3 hidden md:block"
       />
       <img
         src={require("../../Assets/HomePlanets/ast.png")}
         alt=""
         id="planet-1"
-        className="w-[200px] end-12 top-28 -z-30 ast"
+        className="w-[200px] end-12 top-28 ast"
       />
       <img
         src={require("../../Assets/HomePlanets/2.png")}
         alt=""
         id="planet-2"
-        className="w-[350px] left-[30px] top-80 planet-orbit2"
+        className="w-[350px] left-[30px] top-80 planet-orbit2 hidden md:block"
       />
       <div className="flex w-full h-screen items-center justify-center absolute z-[2000]">
         <p className="text-white font-bold text-3xl md:text-6xl ">
@@ -99,8 +101,22 @@ function BgAnimation() {
         src={require("../../Assets/HomePlanets/3.png")}
         alt=""
         id="planet-3"
-        className="w-[150px] end-[30px] top-1/2 planet-orbit1"
+        className="w-[150px] end-[30px] top-1/2 planet-orbit1 hidden md:block"
       />
+    </div>
+  );
+}
+
+
+function GoToPersonalCheck(){
+  return (
+    <div className="w-full h-1/4 bg-black mx-0 p-10 border-y-2 border-dotted border-black hover:border-white hover:border-opacity-60 transition-all duration-300 ease-in-out">
+      <div className="flex justify-center items-center gap-5">
+        <p className="text-center text-white py-0 my-0">
+          Find your most matched planet 🌍
+        </p>
+        <Button variant="contained">Personal Check</Button>
+      </div>
     </div>
   );
 }
